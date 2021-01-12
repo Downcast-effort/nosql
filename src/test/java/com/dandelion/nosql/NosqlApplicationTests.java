60a84765a85b53d2a7aebc2a26ce5a0a783ec1df
imooc <18852895687@163.com>
2021-01-12 08:11:59 +0800

    Initial commit

---
+++ src/test/java/com/dandelion/nosql/NosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dandelion.nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
